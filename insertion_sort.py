5bae1d6d7f9cf88ba2b1d2c28a1f23036d051a53
Arun Pottekat <apottekat@gmail.com>
2017-08-15 18:55:42 +0530

    Linear Search using Python3

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,18 +1,23 @@
 # Insertion Sort 
 
-arr = []
-for i in range(0,10):
-	arr.append(10 - i)
+arr = [31, 41, 59, 26, 41, 58]
 
 def insertionSort(arr):
 	for j in range(1, arr.__len__()):
 		key = arr[j]
 		i = j - 1
+		print("while loop")
 		while arr[i] > key and i >= 0:
 			arr[i+1] = arr[i]
 			i = i - 1
+			print(arr)
 
 		arr[i+1] = key
+		print("\nfor loop")
+		print(arr)
+		print()
+
+	print("\nEnd Insertion sort")
 	print(arr)
 
 insertionSort(arr)
